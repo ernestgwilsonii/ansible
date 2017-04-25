8128fb224d5961a022d3d7a1ab9cfcfdb0752785
ernestgwilsonii <ernestgwilsonii@gmail.com>
2017-04-25 06:51:39 -0400

    Added Ansible for DietPi

---
+++ abrd_dietpi.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+####################################################################################################
+# Ansible Bootstrap Rapid Deployment for Raspbeery Pi running DietPi                               #
+#                                                                                                  #
+# DietPi Usage:                                                                                    #
+#   curl https://raw.githubusercontent.com/ernestgwilsonii/ansible/master/abrd_dietpi.sh | bash -  #
+#                                                                                                  #
+#                                                             ErnestGWilsonII@gmail.com 2016-12-31 #
+#                                                               https://github.com/ernestgwilsonii #
+#                                                      https://www.linkedin.com/in/ernestgwilsonii #
+####################################################################################################
+
+# Update the base OS
+apt-get update
+apt-get upgrade
+
+apt-get -y install python-setuptools gcc libffi-dev python-dev libssl-dev libkrb5-dev git sshpass tree
+
+# Install Python pip
+easy_install pip
+
+# Install some Python module pre-requisites
+pip install sphinx
+pip install --upgrade setuptools
+pip install pybuilder
+pip install mock
+pip install nose
+pip install coverage
+pip install cryptography
+pip install xmltodict
+pip install "pywinrm>=0.1.1"
+pip install kerberos
+pip install httplib2
+
+# Install Ansible
+pip install ansible
+pip install --upgrade ansible
+
+# Create default sample examplews starting kit
+mkdir -p /etc/ansible
+mkdir -p /etc/ansible/files/
+mkdir -p /etc/ansible/group_vars/
+mkdir -p /etc/ansible/host_vars/
+mkdir -p /etc/ansible/templates/
+mkdir -p /etc/ansible/vault/
+echo "ChangeMeToWhateverYouWantForYourAnsibleVaultEncryptionPassword" > /etc/ansible/vault/vault_pass.txt
+echo "hosts" > /etc/ansible/.gitignore
+echo "vault" >> /etc/ansible/.gitignore
+echo "ansible_ssh_user: \"root\"" > /etc/ansible/group_vars/CentOS7.yml
+echo "ansible_ssh_pass: \"YourCentOS7ServersGrouprootPasswordGoesHere\"" >> /etc/ansible/group_vars/CentOS7.yml
+echo "ansible_ssh_port: \"22\"" >> /etc/ansible/group_vars/CentOS7.yml
+echo "ansible_connection: \"ssh\"" >> /etc/ansible/group_vars/CentOS7.yml
+cd /etc/ansible
+wget https://raw.githubusercontent.com/ernestgwilsonii/ansible/master/abrd_dietpi.sh
+
+# Create a default starting /etc/ansible/ansible.cfg
+echo "[defaults]" > /etc/ansible/ansible.cfg
+echo "inventory=/etc/ansible/hosts" >> /etc/ansible/ansible.cfg
+echo "host_key_checking=False" >> /etc/ansible/ansible.cfg
+echo "retry_files_enabled=False" >> /etc/ansible/ansible.cfg
+echo "forks=100" >> /etc/ansible/ansible.cfg
+
+# Add Ansible section to /root/.bashrc
+echo " " >> /root/.bashrc
+echo "# Ansible #" >> /root/.bashrc
+echo "###########" >> /root/.bashrc
+
+# Add ANSIBLE_HOST_KEY_CHECKING=False variable to .bashrc
+echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> /root/.bashrc
+
+# Add ANSIBLE_VAULT_PASSWORD_FILE=/etc/ansible/vault/vault_pass.txt variable to .bashrc
+echo "export ANSIBLE_VAULT_PASSWORD_FILE=/etc/ansible/vault/vault_pass.txt" >> /root/.bashrc
+echo " " >> /root/.bashrc
+
+# Verify your Ansible version
+clear
+echo "################################################################################"
+echo "# Welcome to Ansible! #"
+echo "#######################"
+echo ""
+echo "ansible --version"
+ansible --version
+echo ""
+tree /etc/ansible/
