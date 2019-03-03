1d030db8440a53d767b968e01139ef286ff117c7
ernii <ernii@amazon.com>
2019-03-03 08:17:09 -0500

    Added Raspberry Pi Ansible

--- abrd_raspbian.sh
+++ abrd_raspbian.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
 ####################################################################################################
-# Ansible Bootstrap Rapid Deployment for Raspbeery Pi running DietPi                               #
+# Ansible Bootstrap Rapid Deployment for Raspberry Pi running Raspbian                             #
+#                                        Raspbian: https://www.raspberrypi.org/downloads/raspbian/ #
 #                                                                                                  #
-# DietPi Usage:                                                                                    #
-#   curl https://raw.githubusercontent.com/ernestgwilsonii/ansible/master/abrd_dietpi.sh | bash -  #
+# Raspbian Usage:                                                                                  #
+#   curl https://raw.githubusercontent.com/ernestgwilsonii/ansible/master/abrd_raspbian.sh |bash - #
 #                                                                                                  #
-#                                                             ErnestGWilsonII@gmail.com 2016-12-31 #
+#                                                             ErnestGWilsonII@gmail.com 2019-03-03 #
 #                                                               https://github.com/ernestgwilsonii #
 #                                                      https://www.linkedin.com/in/ernestgwilsonii #
 ####################################################################################################
 
 # Update the base OS
+sudo bash
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
 
 # Install some basic OS requirements for a successful Ansible kit
 apt-get -y install aptitude python-setuptools gcc libffi-dev python-dev libssl-dev libkrb5-dev git sshpass tree
 
+# Note: Raspbian currently has Python 2.7 which is OK for now, but:
+# Python 2.7 DEPRECATION: Python 2.7 will reach the end of its life on January 1st, 2020. 
+# Please upgrade your Python as Python 2.7 won't be maintained after that date. 
+# A future version of pip will drop support for Python 2.7.
+
 # Install Python pip
 easy_install pip
+#pip install --upgrade pip # <--Don't upgrade right now 19.x pip cannot install Ansible!
+#pip install pip==10.0.1 # This version works (if you already upgraded, then downgrade!)
 
 # Install some Python module Ansible pre-requisites
 pip install sphinx
 pip install --upgrade setuptools
-pip install pybuilder
+pip install --upgrade pybuilder
 pip install mock
 pip install nose
 pip install coverage
@@ -35,6 +44,8 @@ pip install kerberos
 pip install httplib2
 
 # Install Ansible
+pip install --upgrade pip setuptools
+pip install cryptography
 pip install ansible
 pip install --upgrade ansible
 
@@ -52,17 +63,17 @@ echo "localhost ansible_host=127.0.0.1" >> /etc/ansible/hosts
 echo " " >> /etc/ansible/hosts
 
 # Create a starting /etc/ansible/vault/vault_pass.txt file
-echo "YourDesiredVaultPasswordGoesHere" >> /etc/ansible/vault/vault_pass.txt
+echo "raspberry" >> /etc/ansible/vault/vault_pass.txt
 
 # Create a starting /etc/ansible/group_vars/raspberrypi.yml file
-echo 'ansible_ssh_user: "root"' >> /etc/ansible/group_vars/raspberrypi.yml
-echo 'ansible_ssh_pass: "dietpi"' >> /etc/ansible/group_vars/raspberrypi.yml
+echo 'ansible_ssh_user: "pi"' >> /etc/ansible/group_vars/raspberrypi.yml
+echo 'ansible_ssh_pass: "raspberry"' >> /etc/ansible/group_vars/raspberrypi.yml
 echo 'ansible_ssh_port: "22"' >> /etc/ansible/group_vars/raspberrypi.yml
 echo 'ansible_connection: "ssh"' >> /etc/ansible/group_vars/raspberrypi.yml
 
 # Create a starting /etc/ansible/host_vars/localhost.yml file
-echo 'ansible_ssh_user: "root"' >> /etc/ansible/host_vars/localhost.yml
-echo 'ansible_ssh_pass: "dietpi"' >> /etc/ansible/host_vars/localhost.yml
+echo 'ansible_ssh_user: "pi"' >> /etc/ansible/host_vars/localhost.yml
+echo 'ansible_ssh_pass: "raspberry"' >> /etc/ansible/host_vars/localhost.yml
 echo 'ansible_ssh_port: "22"' >> /etc/ansible/host_vars/localhost.yml
 echo 'ansible_connection: "ssh"' >> /etc/ansible/host_vars/localhost.yml
 
@@ -71,7 +82,7 @@ echo "vault" >> /etc/ansible/.gitignore
 
 # Pull down any additional starter playbooks
 cd /etc/ansible
-wget https://raw.githubusercontent.com/ernestgwilsonii/ansible/master/DietPi_Apply-OS-Updates-playbook.yml
+wget https://raw.githubusercontent.com/ernestgwilsonii/ansible/master/RaspbianPi_Apply-OS-Updates-playbook.yml
 
 # Create a default starting /etc/ansible/ansible.cfg
 echo "[defaults]" > /etc/ansible/ansible.cfg
